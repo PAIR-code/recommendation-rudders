Fixing neighbor id computation

hyperbolic-rs/rudders/losses.py
@@ -57,8 +57,12 @@ class SemanticLoss(PairwiseHingeLoss):
         super(SemanticLoss, self).__init__(n_users, n_items, args)
         self.item_distances = tf.keras.backend.constant(kwargs["item_distances"])
         # Keep small proportion of closest neighbors of each item to compute loss only over those
-        neighs_ids = tf.math.top_k(-self.item_distances, k=int(len(self.item_distances) * args.neighbors))[1]
+        # First set unreachable nodes to a large distance, then it looks for the indexes of min_k
+        # (top_k of -distances) to use them in the loss calculation
+        valid_item_dists = tf.where(self.item_distances > 0, self.item_distances, tf.keras.backend.ones(1) * 1000)
+        neighs_ids = tf.math.top_k(-valid_item_dists, k=int(len(self.item_distances) * args.neighbors))[1]
         self.neighbor_ids = tf.cast(neighs_ids, tf.int64)
+
         self.distortion_gamma = tf.Variable(args.distortion_gamma * tf.keras.backend.ones(1), trainable=False)
         self.distortion_neg_sample_size = args.distortion_neg_sample_size
 
@@ -84,7 +88,7 @@ class SemanticLoss(PairwiseHingeLoss):
 
             this_loss = (space_distance / graph_distance)**2
             this_loss = tf.abs(this_loss - 1)
-            loss = loss + tf.reduce_mean(this_loss)      # TODO: check tf.reduce_sum
+            loss = loss + tf.reduce_mean(this_loss)
         return loss
 
     def get_dst_index(self, src_index):