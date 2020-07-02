1158a74723a2e1fc535ca901515c92230286b4c7
Federico Lopez <fedelopez77@gmail.com>
2020-07-02 11:16:51 +0000

    Adding license and copyright

--- hyperbolic-rs/rudders/losses.py
+++ hyperbolic-rs/rudders/losses.py
@@ -1,3 +1,16 @@
+# Copyright 2017 The Rudders Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Loss functions for CF with support for optional negative sampling."""
 
 import abc
@@ -53,7 +66,7 @@ class PairwiseHingeLoss(LossFunction):
 
 
 class HingeDistortionLoss(PairwiseHingeLoss):
-    """This loss can only be used with DistanceDistortionHyperbolic since the model need to implement
+    """This loss can only be used with DistanceDistortionHyperbolic since the model needs to implement
         the 'distortion' method"""
 
     def __init__(self, n_users, n_items, args):
