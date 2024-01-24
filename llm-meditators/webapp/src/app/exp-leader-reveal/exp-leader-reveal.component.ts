6017f57397a2446b5105caa632d5bac88c62fedf
Michael Xieyang Liu <lxieyang@umich.edu>
2024-01-24 02:23:54 -0500

    simple calculation of leader

--- llm-meditators/webapp/src/app/exp-leader-reveal/exp-leader-reveal.component.ts
+++ llm-meditators/webapp/src/app/exp-leader-reveal/exp-leader-reveal.component.ts
@@ -1,5 +1,7 @@
 import { Component, Signal, computed } from '@angular/core';
 import { SavedDataService } from '../services/saved-data.service';
+import { Votes } from 'src/lib/staged-exp/data-model';
+import { sortBy, reverse } from 'lodash';
 
 @Component({
   selector: 'app-exp-leader-reveal',
@@ -10,11 +12,43 @@ import { SavedDataService } from '../services/saved-data.service';
 })
 export class ExpLeaderRevealComponent {
   public everyoneReachedTheEnd: Signal<boolean>;
+  public finalLeader: Signal<string>;
 
   constructor(private dataService: SavedDataService) {
     this.everyoneReachedTheEnd = computed(() => {
       const users = Object.values(this.dataService.data().experiment.participants);
       return users.map((userData) => userData.futureStageNames.length).every((n) => n === 0);
+    });
+
+    this.finalLeader = computed(() => {
+      const users = Object.values(this.dataService.data().experiment.participants);
+      const votes: { [userId: string]: number } = {};
+      users.forEach(({ userId }) => {
+        votes[userId] = 0;
+      });
+
+      for (const user of users) {
+        const leaderVotes = user.stageMap['8. Vote for the leader'].config as Votes;
+        for (const userId of Object.keys(leaderVotes)) {
+          const vote = leaderVotes[userId];
+          if (vote === 'positive') {
+            votes[userId] += 1;
+          } else if (vote === 'negative') {
+            votes[userId] -= 1;
+          } else if (vote === 'neutral') {
+            votes[userId] += 0;
+          }
+        }
+      }
+
+      const sorted = reverse(
+        sortBy(
+          Object.entries(votes).map(([userId, vote]) => ({ userId, vote })),
+          ['vote'],
+        ),
+      );
+
+      return sorted[0].userId;
     });
   }
 }
