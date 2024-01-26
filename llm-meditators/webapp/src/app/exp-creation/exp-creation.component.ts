43e77bf166d9379a1ea5fb4174e0e5dccf21a273
Michael Xieyang Liu <lxieyang@umich.edu>
2024-01-26 17:13:42 -0500

    mock up new workflow

--- llm-meditators/webapp/src/app/exp-creation/exp-creation.component.ts
+++ llm-meditators/webapp/src/app/exp-creation/exp-creation.component.ts
@@ -5,20 +5,44 @@ import { makeStages } from 'src/lib/staged-exp/example-experiment';
 import { Component, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { LocalService } from '../services/local.service';
 import { SavedDataService } from '../services/saved-data.service';
 
+const EXISTING_STAGES_KEY = 'existing-stages';
+
 const CREATED_STAGES_KEY = 'created-stages';
+
+const getInitStageData = (): Partial<ExpStage> => {
+  return { complete: false };
+};
 
 @Component({
   selector: 'app-exp-creation',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, NgJsonEditorModule],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    NgJsonEditorModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './exp-creation.component.html',
   styleUrl: './exp-creation.component.scss',
 })
 export class ExpCreationComponent {
+  // new stuff
+  public existingStages: Partial<ExpStage>[] = [];
+  public currentEditingStageIndex = -1;
+  public workingStageData: object = {};
+
+  // old stuff
   public editorOptions: JsonEditorOptions;
 
   public oracleStages = makeStages();
@@ -30,12 +54,32 @@ export class ExpCreationComponent {
   readonly ExpStageNames = ExpStageNames;
   readonly stageKinds = stageKinds;
 
+  readonly availableStageKinds = [
+    stageKinds.STAGE_KIND_TOS_AND_PROFILE,
+    stageKinds.STAGE_KIND_SURVEY,
+    stageKinds.STAGE_KIND_VOTES,
+    stageKinds.STAGE_KIND_CHAT,
+    stageKinds.STAGE_KIND_LEADER_REVEAL,
+  ];
+
   @ViewChild('editor') editor: JsonEditorComponent = new JsonEditorComponent();
 
   constructor(
     private dataService: SavedDataService,
     private localStore: LocalService,
   ) {
+    // new stuff
+    const existingStages = this.localStore.getData(EXISTING_STAGES_KEY) as ExpStage[];
+    if (existingStages) {
+      this.existingStages = existingStages;
+    } else {
+      this.existingStages = [getInitStageData()];
+      this.localStore.saveData(EXISTING_STAGES_KEY, this.existingStages);
+    }
+    this.currentEditingStageIndex = 0;
+    this.workingStageData = structuredClone(this.existingStages[this.currentEditingStageIndex]);
+
+    // old stuff
     const createdStages = this.localStore.getData(CREATED_STAGES_KEY) as ExpStage[];
     if (createdStages) {
       this.createdStages = createdStages;
@@ -51,6 +95,33 @@ export class ExpCreationComponent {
     this.editorOptions.mainMenuBar = false;
   }
 
+  get currentEditingStage() {
+    return this.existingStages[this.currentEditingStageIndex];
+  }
+
+  addNewStage() {
+    this.existingStages.push(getInitStageData());
+    this.localStore.saveData(EXISTING_STAGES_KEY, this.existingStages);
+
+    this.currentEditingStageIndex = this.existingStages.length - 1;
+    this.workingStageData = structuredClone(this.existingStages[this.currentEditingStageIndex]);
+  }
+
+  resetExistingStages() {
+    this.localStore.removeData(EXISTING_STAGES_KEY);
+    this.existingStages = [getInitStageData()];
+    this.localStore.saveData(EXISTING_STAGES_KEY, this.existingStages);
+
+    this.currentEditingStageIndex = 0;
+    this.workingStageData = structuredClone(this.existingStages[this.currentEditingStageIndex]);
+  }
+
+  /**
+   *
+   * old
+   * stuff
+   *
+   */
   get currentStage() {
     return this.createdStages[this.creationIndex];
   }
