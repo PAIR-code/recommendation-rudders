2c56dd5ad6be23c746ead2279b78f63fdbc7518c
Michael Xieyang Liu <lxieyang@umich.edu>
2024-01-26 15:08:13 -0500

    make route for create-experiment

--- llm-meditators/webapp/src/app/app-routing.module.ts
+++ llm-meditators/webapp/src/app/app-routing.module.ts
@@ -8,18 +8,21 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AppHomeComponent as AppHomeComponent } from './app-home/app-home.component';
 import { AppSettingsComponent } from './app-settings/app-settings.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ExpSurveyComponent } from './exp-survey/exp-survey.component';
+import { ExpCreationComponent } from './exp-creation/exp-creation.component';
 import { ExpLeaderVoteComponent } from './exp-leader-vote/exp-leader-vote.component';
+import { ExpSurveyComponent } from './exp-survey/exp-survey.component';
 import { ExpTosComponent } from './exp-tos/exp-tos.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: AppHomeComponent, pathMatch: 'full' },
   { path: 'survey', component: ExpSurveyComponent, pathMatch: 'full' },
   { path: 'leader-vote', component: ExpLeaderVoteComponent, pathMatch: 'full' },
   { path: 'settings', component: AppSettingsComponent, pathMatch: 'full' },
+  { path: 'create-experiment', component: ExpCreationComponent, pathMatch: 'full' },
   { path: 'tos', component: ExpTosComponent, pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
